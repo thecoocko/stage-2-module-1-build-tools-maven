be8e5628989dd5dfd096bc758f8fc07a85c56590
thecoocko <butenkonaddezhda@gmail.com>
2023-08-22 13:25:44 +0200

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,14 @@
 package com.epam.demo;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        List<Boolean> results = new LinkedList<>();
+        for(String i : args){
+            results.add(i.startsWith("-"));
+        }
+        return results.contains(true);
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,14 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try {
+            if (str.startsWith("0") && str.charAt(1)!='.'){
+                return false;
+            }
+            return Double.valueOf(str) > 0 ? true : false;
+        }
+        catch (Exception e){
+            return false;
+        }
     }
 }
